a1cc108f3cef1638b42f7e5223dcfc1b11931f5d
Aurélie <aurelie.wagner@betc.com>
2020-01-21 17:19:59 +0100

    pb reload

--- src/Pages/Exo/Exo3.js
+++ src/Pages/Exo/Exo3.js
@@ -1,17 +1,35 @@
-import React, {useReducer} from "react";
+import React, {useReducer, useState} from "react";
 import { Link} from "react-router-dom"
 
 const initialState = {
     moyenne: 0,
-    notes: [13,8],
+    notes: [],
+    currentNote: 0,
 
 };
 
 const reducer = (state, action) => {
+
     switch (action.type) {
         case "notes":
-            return {...state, notes: state.notes.push(action.payload)};
-
+            if(state.notes.length >= 10){
+                alert("Vous ne pouvez saisir que 10 notes max");
+            }else{
+                let newTab = state.notes.push( parseInt(state.currentNote));
+                console.log("newTab", newTab);
+                return {...state, notes: newTab};
+            }
+        case "moyenne" :
+            if(state.notes){
+                let total = 0;
+                for(let i = 0; i<state.notes.lenght; i++){
+                    console.log("coucou");
+                    total += state.notes[i];
+                }
+                return {...state, moyenne: total};
+            };
+        case "saveCurrentNote":
+            return {...state, currentNote: action.payload};
         default :
             alert("ereur");
     }
@@ -23,11 +41,13 @@ const reducer = (state, action) => {
 export default function Exo2() {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+
     const generateNotes=()=>{
-        console.log(initialState)
-        for(let i=0; i< initialState.notes.length; i++){
-            return initialState.notes[i];
-        }
+        let boucle = initialState.notes.map((item, i) =>
+                    item + ", "
+             )
+        console.log(boucle);
+        // return boucle;
     }
 
     return (
@@ -38,17 +58,21 @@ export default function Exo2() {
             <br/>
             <br/>
 
-            <form onSubmit={() => dispatch({ type: "notes" })} >
+            <form onSubmit={(event) => dispatch({ type: "notes" })} >
                 <span> Saisir une note </span>
-                <input id={"value"} type="number" min="0" max="20" />
+                <input id={"value"} type="number" min="0" max="20" onChange={(event) => dispatch({ type: "saveCurrentNote", payload: event.target.value })} />
                 <button type="submit">Ajouter la note</button>
             </form>
 
-            <div>{generateNotes()}</div>
+            <br/>
+            <br/>
+            <div>Voici les notes : {generateNotes()}</div>
 
-
+            <br/>
+            <br/>
+            <button onClick={() => dispatch({ type: "moyenne" })}>Calculer la moyenne</button>
             <span>Result : {state.moyenne} </span>
         </>
-)
+    )
 }
 
